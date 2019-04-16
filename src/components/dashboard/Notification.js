2b626e7d29cee717736d64ba0fe11227e619e063
soap51 <soap51@outlook.com>
2019-04-16 17:21:11 +0700

    Mock UI

---
+++ src/components/dashboard/Notification.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Notification = () => {
+    return (
+        <div>
+            <p>Notifications</p>
+        </div>
+    )
+}
+export default Notification
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import SignedInLink from './SignedInLink'
+import SignedOutLink from './SignedOutLink'
+const  Navbar = () => {
+    return (
+        <nav className="nav nav-wrapper gray darken-3">
+            <div className="container">
+                <Link to="/" className="brand-logo">MarioPlan</Link>
+                <SignedInLink/>
+                <SignedOutLink/>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
